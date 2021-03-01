a4915a968bb123c754b734383ebbdf21c7c846a2
Spay Shawl <fournier.alexis.73@gmail.com>
2021-03-01 10:18:36 +0100

    feat(js): create medias components

--- js/components/medias/Image.js
+++ js/components/medias/Image.js
@@ -1,20 +1,32 @@
 import "./../../../css/components/medias/image.css";
 
+/**Component representing a Media's image */
 export default class Image{
+    /**
+     * Create a Media's image
+     * @param photographerName {string}
+     * @param media {Media} the media tu display
+     */
     constructor(photographerName, media) {
         this._name = photographerName;
         this._media = media;
     }
 
     /**
-     *
+     * Create and return the element
      * @returns {HTMLImageElement}
      */
     createElement(){
         const image = document.createElement('img');
-        image.src = "../src/photographers/" + this._name + "/content/" + this._media.image;
+        const labelBy = this._media.description.toLowerCase().replaceAll(" ", "-");
+
+        image.src = "../src/photographers/" + this._name + "/Content/" + this._media.image;
         image.alt = this._media.description;
         image.className = "image";
+        image.tabIndex = 1;
+        image.setAttribute("role", "button");
+        image.setAttribute("aria-labelledby",labelBy + "-label");
+
         return image;
     }
 }
--- js/components/medias/Title.js
+++ js/components/medias/Title.js
@@ -1,6 +1,24 @@
+/**Component representing a Media title */
 export default class Title{
-    constructor(){
-
+    /**
+     * Create a Media title
+     * @param title {string}
+     */
+    constructor(title){
+        this._title = title;
     }
 
+    /**
+     * Create and return the element
+     * @returns {HTMLParagraphElement}
+     */
+    createElement(){
+        const title = document.createElement('p');
+
+        title.className = "title";
+        title.id = this._title.toLowerCase().replaceAll(" ", "-") + "-label";
+        title.textContent = this._title;
+
+        return title;
+    }
 }
