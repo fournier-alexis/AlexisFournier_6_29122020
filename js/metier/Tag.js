4167c81fde0dc7b1635cac88b88ef89c5b37488f
Spay Shawl <fournier.alexis.73@gmail.com>
2021-03-01 10:17:36 +0100

    feat(js): create logic layer

--- js/metier/Tag.js
+++ js/metier/Tag.js
@@ -1,4 +1,9 @@
+/**Class representing a point */
 export default class Tag {
+    /**
+     * Create a tag
+     * @param label {string}
+     */
     constructor(label) {
         this.label = label;
     }
